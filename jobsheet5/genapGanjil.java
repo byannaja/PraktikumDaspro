8a6a57e0e092e8d0c49133f41cfd8cecc5e97262
gbyn <byank2314@gmail.com>
2025-10-12 18:45:23 +0700

    Tugas 2

---
+++ jobsheet5/genapGanjil.java
@@ -0,0 +1,20 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class genapGanjil {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int angka;
+
+        System.out.print("Masukkan angka: ");
+        angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+
+        sc.close();
+    }
+}
