8e18ae05f913ecf88e4b690e95eeb757653f9fa2
gbyn <byank2314@gmail.com>
2025-11-04 13:43:11 +0700

    percobaan 3

---
+++ jobsheet8/Triangle18.java
@@ -0,0 +1,23 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class Triangle18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+
+        int i = 1; 
+        while (i <= n) {
+            int j = 1;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+        sc.close();
+    }
+}
