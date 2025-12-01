0e944d5723cb54ff2fa14c0017ac5936b35e24ba
gbyn <byank2314@gmail.com>
2025-12-01 09:13:54 +0700

    ModPercobaan1

--- Jobsheet11/bioskop18.java
+++ Jobsheet11/bioskop18.java
@@ -22,10 +22,9 @@ public class bioskop18 {
         for (String[] barisPenonton : penonton) {
         System.out.println("Panjang baris: " + barisPenonton.length);
 
-        System.out.println("Penonton pada baris ke-3: ");
-        for (int i = 0; i < penonton[2].length; i++) {
-        System.out.println(penonton[2][i]);
+        for (int i = 0; i < penonton.length; i++) {
+        System.out.println("Penonton pada baris ke-" + (i + 1) + ":" + String.join(", ", penonton[i]));
 }
 }
 }
-    }
+}
