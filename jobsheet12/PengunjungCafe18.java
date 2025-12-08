9c2820635b9a6625e4109ec68d25c95d7112e178
gbyn <byank2314@gmail.com>
2025-12-08 11:14:58 +0700

    Percobaan 4

---
+++ jobsheet12/PengunjungCafe18.java
@@ -0,0 +1,21 @@
+package jobsheet12;
+
+public class PengunjungCafe18 {
+
+public class PengunjungCafeNoAbsen {
+
+    static void daftarPengunjung(String...namaPengunjung){
+        System.out.println("Daftar Nama Pengunjung:");
+        
+        for(int i = 0; i < namaPengunjung.length; i++){
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali","Budi","Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni","Eti","Fahmi","Galih");
+    }
+}
+}
